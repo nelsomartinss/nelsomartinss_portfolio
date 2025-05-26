c9b7936c7bab604443d5d7ceeab785a91462df58
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-05-26 20:55:30 -0300

    seção de projetos finalizada

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,10 +1,12 @@
 function ProjectCard({ src, link, h3, p }) {
   return (
-    <a href={link} target='_blank'>
-        <img className="hover" src={src} alt={`${h3} logo`} />
-        <h3>{h3}</h3>
-        <p>{p}</p>
-    </a>
+        <a href={link} target='_blank' className='card'>
+            <img className="hover" src={src} alt={`${h3} logo`} />
+            <h3>{h3}</h3>
+            <div className='descriptionCard'>
+              <p>{p}</p>
+            </div>
+        </a>
   )
 }
 
--- src/sections/SectionProjects/Projects.jsx
+++ src/sections/SectionProjects/Projects.jsx
@@ -5,17 +5,19 @@ import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
   return <section id='projects' className={styles.container}>
-    <h1 className={styles.sectionTitle}>Projects</h1>
+    <h1 className={styles.sectionTitle}>Projetos</h1>
     <div className={styles.projectsContainer}>
       <ProjectCard 
       src={todolistApp} 
       link='https://todolistnelsonmartins.netlify.app/'
       h3='To-do List'
+      p='Uma To-do List que utiliza o LocalStorage do navegador para restaurar as tarefas criadas e marcadas.'
       />
       <ProjectCard 
       src={linktree} 
       link='https://somaestudiolinks.netlify.app/'
-      h3='Linktree - Soma Estúdio'
+      h3='Linktree'
+      p='Criei um Linktree personalizado para o Soma Estúdio com identidade visual própria e diferente do convencional.'
       />
     </div>
   </section>
